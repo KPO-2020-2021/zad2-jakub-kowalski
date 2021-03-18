4f48a053251f0cc8196973857cf17eaf8bbc0664
Jakub Kowalski <258959@student.pwr.edu.pl>
2021-03-18 20:06:09 +0100

    Dodano funkcje obliczajaca wynik procentowy

--- src/Statystyki.cpp
+++ src/Statystyki.cpp
@@ -1,7 +1,20 @@
+#include <iostream>
 #include "Statystyki.hh"
 
 
-/*
- * Tu nalezy zdefiniowac funkcje, ktorych zapowiedzi znajduja sie
- * w pliku naglowkowym.
- */
+/*!
+* Podaje statystyki testu
+* Argumenty:
+*   poprawne - ilosc poprawnych odpowiedzi
+*   niepoprawne - ilosc blednych odpowiedzi
+* Zwraca:
+*   Liczbe poprawnych i blednych odpowiedzi oraz procent poprawnych odpowiedzi
+*/
+void statystyki(int poprawne, int niepoprawne)
+{
+    int wynik;
+    std::cout << "Ilosc poprawnych odpowiedzi: " << poprawne << std::endl;
+    std::cout << "Ilosc blednych odpowiedzi: " << niepoprawne << std::endl;
+    wynik = poprawne / 4 * 100;
+    std::cout << "Procent poprawnych odpowiedzi: " << wynik << "%" << std::endl;
+}
